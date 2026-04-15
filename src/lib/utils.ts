50022d57eee27396cdc4a33b0fd009203ae97905
PEV-tech <maxine@private-equity-valley.com>
2026-04-15 10:53:52 +0200

    feat: add fixEncoding() shared utility to lib/utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,3 +16,28 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * Fix UTF-8 triple-corruption. Use instead of any singleFix pattern.
+ * Takes a raw binary string (from atob) and returns proper Unicode.
+ */
+export function fixEncoding(rawBinary: string): string {
+  const bytes = new Uint8Array(rawBinary.length)
+  for (let i = 0; i < rawBinary.length; i++) bytes[i] = rawBinary.charCodeAt(i)
+  let text = new TextDecoder('utf-8', { fatal: false }).decode(bytes)
+  text = text
+    .replace(/\u00E2\u0080\u0094/g, '\u2014')
+    .replace(/\u00E2\u0080\u0099/g, '\u2019')
+    .replace(/\u00C3\u00A9/g, '\u00E9')
+    .replace(/\u00C3\u00A0/g, '\u00E0')
+    .replace(/\u00C3\u00A8/g, '\u00E8')
+    .replace(/\u00C3\u00AA/g, '\u00EA')
+    .replace(/\u00C3\u00AE/g, '\u00EE')
+    .replace(/\u00C3\u00B4/g, '\u00F4')
+    .replace(/\u00C3\u00BB/g, '\u00FB')
+    .replace(/\u00C3\u00A7/g, '\u00E7')
+    .replace(/\u00C3\u00A2/g, '\u00E2')
+    .replace(/\u00E2\u0080\u00A6/g, '\u2026')
+  return text
+}
+
